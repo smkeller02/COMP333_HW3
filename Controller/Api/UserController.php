2bc56a56a195d28714d9e525c93d1b98c47c2048
Sydney Keller <smkeller@wesleyan.edu>
2023-10-23 22:31:43 -0400

    Creating signup user endpoint + Postman testing | Postman Tested Local URI: http://localhost/COMP333_HW3/index.php/createuser (issue #8)

--- Controller/Api/UserController.php
+++ Controller/Api/UserController.php
@@ -1,9 +1,63 @@
 <?php
 class UserController extends BaseController
 {
-    function log($message) {
-        $log_file = "/Applications/XAMPP/logs/HW3debugging-log.log";
-        error_log($message . "\n", 3, $log_file);
+    public function createuserAction() {
+        // Get the request method (e.g., GET, POST, PUT, DELETE)
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $arrQueryStringParams = $this->getQueryStringParams();
+        // Check if the request method is POST
+        if (strtoupper($requestMethod) == 'POST') {
+            try {
+                // Retrieve user regustration data from the request body
+                $postData = json_decode(file_get_contents('php://input'), true);
+                if (!(array_key_exists('username', $postData) && array_key_exists('password', $postData))) {
+                    $strErrorDesc = "Missing username/password";
+                    $strErrorHeader = 'HTTP/1.1 400 Bad Request';
+                } else {
+                    // All expected input received
+                    $username = $postData["username"];
+                    $password = $postData["password"];
+                    if (strlen($password) < 10) {
+                        $strErrorDesc = "Password less than 10 characters";
+                        $strErrorHeader = 'HTTP/1.1 400 Bad Request';
+                    } else {
+                        // Check if username is already in use
+                        $userModel = new UserModel();
+                        $existsResult = $userModel->checkUserExists($username);
+                        if (!$existsResult) {
+                            $userModel->createUser($username, password_hash($password, PASSWORD_DEFAULT));
+                            $userCreated = true;
+                        } else {
+                            $userCreated = false;
+                        }
+                        // Turn into array for better reading comprehension
+                        $array = [
+                            "userExists" => $existsResult,
+                            "userCreated" => $userCreated
+                        ];
+                        $responseData = json_encode($array);
+                    }
+                }
+            } catch (Exception $e) {
+                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+         // send output
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 200 OK')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)),
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
     }
     public function ratingsAction()
     {
