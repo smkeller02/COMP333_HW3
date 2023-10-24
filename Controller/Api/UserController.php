5afbcab3f9c230332e8341bc7c20e79a1b3cfcaa
Sydney Keller <smkeller@wesleyan.edu>
2023-10-23 21:45:56 -0400

    Deleting backend folder to get files to root (issue #6)

--- Controller/Api/UserController.php
+++ Controller/Api/UserController.php
@@ -1,21 +1,20 @@
 <?php
 class UserController extends BaseController
 {
-    public function listAction()
+    function log($message) {
+        $log_file = "/Applications/XAMPP/logs/HW3debugging-log.log";
+        error_log($message . "\n", 3, $log_file);
+    }
+    public function ratingsAction()
     {
         $strErrorDesc = '';
         $requestMethod = $_SERVER["REQUEST_METHOD"];
-        $arrQueryStringParams = $this->getQueryStringParams();
         if (strtoupper($requestMethod) == 'GET') {
             try {
                 $userModel = new UserModel();
-                $intLimit = 10;
-                if (isset($arrQueryStringParams['limit']) && $arrQueryStringParams['limit']) {
-                    $intLimit = $arrQueryStringParams['limit'];
-                }
-                $arrUsers = $userModel->getUsers($intLimit);
+                $arrUsers = $userModel->getRatings();
                 $responseData = json_encode($arrUsers);
-            } catch (Error $e) {
+            } catch (Exception $e) {
                 $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
                 $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
             }
@@ -23,16 +22,18 @@ class UserController extends BaseController
             $strErrorDesc = 'Method not supported';
             $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
         }
-        // send output 
+        // send output
         if (!$strErrorDesc) {
             $this->sendOutput(
                 $responseData,
                 array('Content-Type: application/json', 'HTTP/1.1 200 OK')
             );
         } else {
-            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)),
                 array('Content-Type: application/json', $strErrorHeader)
             );
         }
     }
 }
+
+?>
